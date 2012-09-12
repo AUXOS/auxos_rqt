cadc2c223f666c707245f241d7aa32a7c4138da2
David Hodo <david.hodo@gmail.com>
2012-09-12 15:59:49 -0500

    initial import

---
+++ path_following_client/src/path_following_client/path_following_client.py
@@ -0,0 +1,39 @@
+import os
+import roslib
+roslib.load_manifest('path_following_client')
+import rospy
+
+from qt_gui.plugin import Plugin
+from qt_gui.qt_binding_helper import loadUi
+from QtGui import QWidget
+
+class PathFollowingClient(Plugin):
+
+    def __init__(self, context):
+        super(MyPlugin, self).__init__(context)
+        # give QObjects reasonable names
+        self.setObjectName('PathFollowingClient')
+
+        # create widget object
+        self._widget = PathFollowingWidget()
+
+        # add widget to the user interface
+        context.add_widget(self._widget)
+
+    def shutdown_plugin(self):
+        # TODO unregister all publishers here
+        pass
+
+    def save_settings(self, plugin_settings, instance_settings):
+        # TODO save intrinsic configuration, usually using:
+        # instance_settings.set_value(k, v)
+        pass
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        # TODO restore intrinsic configuration, usually using:
+        # v = instance_settings.value(k)
+        pass
+
+    #def trigger_configuration(self):
+        # Comment in to signal that the plugin has a way to configure it
+        # Usually used to open a dialog to offer the user a set of configuration
---
+++ path_following_client/src/path_following_client/path_following_widget.py
@@ -0,0 +1,69 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2012, IS4S, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of Willow Garage, Inc. nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import os
+
+from qt_gui.qt_binding_helper import loadUi
+from QtGui import QApplication, QCursor, QFileDialog, QIcon, QMenu, QMessageBox, QTableView, QWidget
+from QtCore import QRegExp, Qt, qWarning
+
+
+class PathFollowingWidget(QWidget):
+    """
+    Primary widget for the rqt_path_following_client plugin.
+    """
+    def __init__(self, proxymodel):
+        super(ConsoleWidget, self).__init__()
+        ui_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'path_following_widget.ui')
+        loadUi(ui_file, self)
+        self.setObjectName('PathFollowingWidget')
+
+        # set up callbacks
+        self.path_filename_button.clicked[bool].connect(self._handle_select_path)
+        self.load_button.clicked[bool].connect(self._handle_start_following)
+        self.save_button.clicked[bool].connect(self._handle_stop_following)
+
+    def _handle_select_path(self, checked):
+        filename = QFileDialog.getOpenFileName(self, self.tr('Load Path from File'), '.', self.tr('Path File {.txt} (*.txt)'))
+        if filename[0] != '':
+            try:
+                handle = open(filename[0])
+            except IOError as e:
+                qWarning(str(e))
+                return
+        path_filename.setText(filename)
+
+    def _handle_start_following(self, checked):
+        pass
+
+    def _handle_stop_following(self, checked):
+        pass
